0806701a45fa14f2f4887eae787fcfeb87a10d84
mark-lutabi <mark.lutabi@mths.ca>
2024-11-01 11:14:07 -0400

    changed comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mark lutabi
+ * Created on: November, 2024
+ * This program move the Sprite and the pixels making a square
 */
 
 
